a4b5f1040712106b3983567162afd7119d3c0677
swiftwithlove <47333157+swiftwithlove@users.noreply.github.com>
2019-02-04 15:39:11 -0500

    Add files via upload
    Xcode project for video: https://www.youtube.com/watch?v=d7fHuNcWMok&feature=youtu.be

---
+++ single-view-template/single-view-template/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  single-view-template
+//
+//  Created by Adam Dahan on 2019-02-04.
+//  Copyright © 2019 Adam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        view.backgroundColor = UIColor.purple
+    }
+}
+
